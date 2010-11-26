0790e0fbb33d7593b7a70289383edd76ce719ed7
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-11-25 16:38:46 -0800

    Finished importing the stock project layout.

---
+++ marrow/blueprint/release.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+"""Release information about Marrow Blueprint."""
+
+
+name = "marrow.blueprint"
+version = "0.1"
+release = "0.1"
+
+summary = "Flexible manifest-driven file and folder creation."
+description = """"""
+author = "Alice Bevan-McGregor"
+email = "alice@gothcandy.com"
+url = "https://github.com/pulp/marrow.blueprint"
+download_url = "http://pypi.python.org/pypi/marrow.blueprint"
+copyright = "2010, Alice Bevan-McGregor"
+license = "MIT"
